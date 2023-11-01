d1aa3e329ad2a4b5a2ffaaa22145aa11026a8768
uvhvy <uvhvy@DESKTOP-OM746DK>
2023-11-01 13:47:16 +0330

    Add project files.

---
+++ mianginn/Program.cs
@@ -0,0 +1,103 @@
+﻿
+//amir hossein hemmati
+// T.1 MIANGIN
+
+double y = 0;
+int i = 0;
+double nomre = (0 - 20);
+int vahed;
+double c = 0;
+double x;
+do
+{
+    Console.WriteLine("nomre dars 1 ra vared konid ");
+    nomre = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("tedad vahed dars 1 ra verd konid");
+    vahed = Convert.ToInt32(Console.ReadLine());
+    x = nomre * vahed;
+    y = y + x;
+    c = c + vahed;
+    i = i + 1;
+    Console.WriteLine("nomre dars 2 ra vared konid ");
+    nomre = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("tedad vahed dars 2 ra verd konid");
+    vahed = Convert.ToInt32(Console.ReadLine());
+    x = nomre * vahed;
+    y = y + x;
+    c = c + vahed;
+    i = i + 1;
+    Console.WriteLine("nomre dars 3 ra vared konid ");
+    nomre = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("tedad vahed dars 3 ra verd konid");
+    vahed = Convert.ToInt32(Console.ReadLine());
+    x = nomre * vahed;
+    y = y + x;
+    c = c + vahed;
+    i = i + 1;
+    Console.WriteLine("nomre dars 4 ra vared konid ");
+    nomre = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("tedad vahed dars 4 ra verd konid");
+    vahed = Convert.ToInt32(Console.ReadLine());
+    x = nomre * vahed;
+    y = y + x;
+    c = c + vahed;
+    i = i + 1;
+    Console.WriteLine("nomre dars 5 ra vared konid ");
+    nomre = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("tedad vahed dars 5 ra verd konid");
+    vahed = Convert.ToInt32(Console.ReadLine());
+    x = nomre * vahed;
+    y = y + x;
+    c = c + vahed;
+    i = i + 1;
+    Console.WriteLine("nomre dars 6 ra vared konid ");
+    nomre = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("tedad vahed dars 6 ra verd konid");
+    vahed = Convert.ToInt32(Console.ReadLine());
+    x = nomre * vahed;
+    y = y + x;
+    c = c + vahed;
+    i = i + 1;
+    Console.WriteLine("nomre dars 7 ra vared konid ");
+    nomre = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("tedad vahed dars 7 ra verd konid");
+    vahed = Convert.ToInt32(Console.ReadLine());
+    x = nomre * vahed;
+    y = y + x;
+    c = c + vahed;
+    i = i + 1;
+    Console.WriteLine("nomre dars 8 ra vared konid ");
+    nomre = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("tedad vahed dars 8 ra verd konid");
+    vahed = Convert.ToInt32(Console.ReadLine());
+    x = nomre * vahed;
+    y = y + x;
+    c = c + vahed;
+    i = i + 1;
+    Console.WriteLine("nomre dars 9 ra vared konid ");
+    nomre = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("tedad vahed dars 9 ra verd konid");
+    vahed = Convert.ToInt32(Console.ReadLine());
+    x = nomre * vahed;
+    y = y + x;
+    c = c + vahed;
+    i = i + 1;
+    Console.WriteLine("nomre dars 10 ra vared konid ");
+    nomre = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("tedad vahed dars 10 ra verd konid");
+    vahed = Convert.ToInt32(Console.ReadLine());
+    x = nomre * vahed;
+    y = y + x;
+    c = c + vahed;
+    i = i + 1;
+    double miangin;
+    miangin = (int)(y / c);
+    Console.WriteLine($"miangin nomarat shoma barabar ast ba:{miangin}");
+
+    string YESorNO;
+    Console.WriteLine("aya mikhahid edame dahid? y/n");
+    YESorNO = Console.ReadLine();
+
+} while (Console.ReadLine() == "y");
+
+
